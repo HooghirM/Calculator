6e8024e017e33c082f635848c134c3ea7d6f94e1
hooghir <hooghirm29@gmail.com>
2024-06-28 17:10:56 -0400

    3rd commit. Addiotnal code to js

--- calculator.js
+++ calculator.js
@@ -1,1 +1,54 @@
-console.log('Hi!')
+// console.log('Hi!')
+
+/* Capture button clicks to handle user input.
+• Update the display area with the current input and result.
+• Perform arithmetic operations when the user clicks the equal button.
+• Clear the display and reset the calculator state when the user clicks the clear button.
+• Handle decimal numbers. */
+
+// On click for Random Digit generator
+document.getElementById('Random').onclick = function(e){
+    // When button clicked
+    // Generate random number 1 in 1st number:
+    // Generate random number 2 in 2nd number:
+
+    // Random number 1/2 instantiated and assigned 0 to start as a value
+    let randomNumber1 = 0
+    let randomNumber2 = 0
+
+    // Math.random() * 10
+    // alert(Math.random() * 9)
+
+    // Generate random number 1 and 2
+    randomNumber1 = (Math.random() * 9)
+    randomNumber2 = (Math.random() * 9)
+
+
+    // Display random number 1 and 2 in the input fields
+     $(h1.Random).text(randomNumber1)
+     $(h1.Random).text(randomNumber2)
+
+     
+    // $(h1.Random).data('randomNumber', randomNumber1)
+    // $(h1.Random).data('randomNumber2', randomNumber2)
+
+
+  }
+
+  document.getElementById('Arithmetic').onclick = function(e){
+        // Randomly generate arithmetic operator
+
+
+  }
+
+  document.getElementById('Calculate').onclick = function(e){
+         // Calculate the current inputs using both random numbers and arithmetic operator 
+
+
+  }
+
+  document.getElementById('Clear').onclick = function(e){
+         // Clear the results and input section
+
+
+  }
