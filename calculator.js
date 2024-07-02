cfe3877a27d21a3817ec7ce8965171be2eb88160
Hooghir Mohammed <hooghirmohammed@Hooghirs-MacBook-Pro-2.local>
2024-07-01 22:19:54 -0400

    refactor: Got rid of original code to start from scratch with new logic

--- calculator.js
+++ calculator.js
@@ -13,65 +13,141 @@ Upon clicking you can store the last number... OR
 Have a list for the last number for the 2nd most recent, 3rd... however
 make another button */
 
+// LOGIC:
+// HAVE INPUT FIELD FOR SELECTING NUMBER 0-9 
+// HAVE 4 buttons for selecting operation
+// Click calculate button to calculate the result
+// Clear button to clear the input fields
 
-                /* Random number generator */
-    document.addEventListener('click', function(event) {
-       if (event.target.id === 'Random') { // Check if the clicked element has the ID 'Random'
+document.addEventListener('click', function(event) {
+           if (event.target.id === '0') {
+
+            /*                  What i need to do            */
+            // if id = '1stNumField'
+            // then display 0 when clicked in 1stNumField
+            // else id is 2ndNumField
+            // then display 0 when clicked in 2ndNumField
+
+            
+
+            // if (document.getElementById('0').textContent === '0') {
+            //     document.getElementById('1stNumField').textContent = '0'
+            // }
+
+
+
+            // if (event.target.id === '1stNumField') {
+            //     document.getElementById('1stNumField').textContent = '0'
+            // } else if (event.target.id === '2ndNumField') {
+            //     document.getElementById('2ndNumField').textContent = '0'
+            // }
+
+           }
+        });
+
+
+
+
+
+
+// let randomNumber1
+// let randomNumber2
+// let randomOperator
+
+//        /* Random number generator */
+//     document.addEventListener('click', function(event) {
+//        if (event.target.id === 'Random') { // Check if the clicked element has the ID 'Random'
           
-        // Varibles containing random numbers between 1 and 9
-           let randomNumber1 = Math.floor(Math.random() * 9) +1
-           let randomNumber2 = Math.floor(Math.random() * 9) +1
+//         // Varibles containing random numbers between 1 and 9
+//            let randomNumber1 = Math.floor(Math.random() * 9) +1
+//            let randomNumber2 = Math.floor(Math.random() * 9) +1
 
-        // Display the random numbers in the input fields
-        FirstNumber(`1st number: ${randomNumber1}`)
-        SecondNumber(`2nd number: ${randomNumber2}`)
+//            // Display the random numbers in the input fields
+//         document.getElementById('1stNumber').textContent = `1st number: ${randomNumber1}`
+//         document.getElementById('2ndNumber').textContent = `2nd number: ${randomNumber2}`
+            
+//         // Have variables for storing most recent random nums for calculation
+    
+//            console.log('Random button clicked');
+//        }
+//        }); 
+
+//     // FUNCTION TO GENERATE RANDOM ARITHMETIC OPERATOR
+//     // MAKE IT 4 different buttons... one for each operator...
+//     // NOT A STRING
+//     document.addEventListener('click', function(event) {
+//         if (event.target.id === 'Arithmetic') {
+
+//             // Have 4 buttons for this so we can actually do the math and it doesn't have
+//             // to be in a string
+
+//         // Variables for arithmetic operators
+//         let add = '+'
+//         let subtract = '-'
+//         let multiply = '*'  
+//         let divide = '/'    
+
+//         // Array of arithmetic operators
+//         let operators = [add, subtract, multiply, divide]
+
+//         // Randomly generate arithmetic operator
+//         for (let index = 0; index < operators.length; index++) {
+//             let randomOperator = operators[Math.floor(Math.random() * operators.length)]
+//             document.getElementById('Arithmetic').textContent = `Arithmetic operator: ${randomOperator}`        
+//             console.log('Arithmetic button clicked'); 
+//             // Have a varable to store the most recent operator for future calculation
+
+//         }
+//     }
+// });
+
+// // let randomNumber1
+// // let randomNumber2
+// // let randomOperator
+// document.addEventListener('click', function(event) {
+//     if (event.target.id === 'Calculate') {
+//     // Calculate the current inputs using both random numbers and arithmetic operator 
+
+//         /* Need to access both current random numbers (randomNumber1/2) 
+//         and arithmetic operator(randomOperator) */
+
+
+
+//         // document.getElementById('Calculate').textContent = `Calculate ${result(randomNumber1, randomNumber2, randomOperator)}`
+
+//         console.log(randomNumber1)
+//         console.log(randomNumber2)
+//         console.log(randomOperator)
+//         console.log('Calculate button clicked')
+
+//         /* Need to calculate the result of the two random numbers using 
+//         the arithmetic operator */
         
-           console.log('Random button clicked');
-       }
-       }); 
+
+//         // Need to display the result in the results section
+        
+
+//   }
+// });
+
+// function result(randomNumber1, randomNumber2, randomOperator){
+//     // Conditional: if (+) rand1 + rand2 else if (-) rand1 - rand2, etc
+//     if (randomOperator === '+'){
+//         return randomNumber1 + randomNumber2
+//     } else if (randomOperator === '-'){
+//         return randomNumber1 - randomNumber2
+//     } else if (randomOperator === '*'){
+//         return randomNumber1 * randomNumber2
+//     } else if (randomOperator === '/'){
+//         return randomNumber1 / randomNumber2
+//     }
 
 
-       /* MAYBE PUT THESE IN RANDOM ON CLICK FUNCTIONs*/
-       // Sets HTML element w/ ID of 1stNumber to value of param randomNum 
-    function FirstNumber(randomNumber1){
-        document.getElementById('1stNumber').textContent = randomNumber1
-    }
-
-    // Sets HTML element w/ ID of 2ndNumber to value of param randomNum
-    function SecondNumber(randomNumber2){
-        document.getElementById('2ndNumber').textContent = randomNumber2
-    }
+// }
+// document.addEventListener('click', function(event) {
+//     if (event.target.id === 'Clear') {
+//          // Clear the results and input section
 
 
-    // FUNCTION TO GENERATE RANDOM ARITHMETIC OPERATOR
-    document.addEventListener('click', function(event) {
-        if (event.target.id === 'Arithmetic') {
-
-        // Variables for arithmetic operators
-        let add = '+'
-        let subtract = '-'
-        let multiply = '*'  
-        let divide = '/'    
-
-        // Array of arithmetic operators
-        let operators = [add, subtract, multiply, divide]
-
-        // Randomly generate arithmetic operator
-        for (let index = 0; index < operators.length; index++) {
-            let randomOperator = operators[Math.floor(Math.random() * operators.length)]
-            document.getElementById('Arithmetic').textContent = `Arithmetic operator: ${randomOperator}`         
-        }
-    }
-});
-
-  document.getElementById('Calculate').onclick = function(e){
-         // Calculate the current inputs using both random numbers and arithmetic operator 
-
-
-  }
-
-  document.getElementById('Clear').onclick = function(e){
-         // Clear the results and input section
-
-
-  }
+//   }
+// });
